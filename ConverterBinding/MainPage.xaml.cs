aed546e3b21e7946645069f80e389baf3e0e6501
Bryce <techbryce@hotmail.com>
2023-07-20 08:04:13 +1000

    ADD - Initial Commit

---
+++ ConverterBinding/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ConverterBinding;
+
+public partial class MainPage
+{
+    public string AccessibilityName { get; set; }
+    public bool IsInAccessibleTree { get; set; }
+    public MainPage()
+    {
+        InitializeComponent();
+        InitializeProperties();
+    }
+
+    private void InitializeProperties()
+    {
+        AccessibilityName = string.Empty;
+        IsInAccessibleTree = true;
+    }
+}
