a916331d9b2ef2428841771a1b192acc8fc03800
Bryce <techbryce@hotmail.com>
2023-07-20 19:33:17 +1000

    ADD - Added MainPageControl.xaml, simplified example

--- ConverterBinding/MainPage.xaml.cs
+++ ConverterBinding/MainPage.xaml.cs
@@ -2,17 +2,9 @@
 
 public partial class MainPage
 {
-    public string AccessibilityName { get; set; }
-    public bool IsInAccessibleTree { get; set; }
+    public string TextValue { get; set; }
     public MainPage()
     {
         InitializeComponent();
-        InitializeProperties();
-    }
-
-    private void InitializeProperties()
-    {
-        AccessibilityName = string.Empty;
-        IsInAccessibleTree = true;
     }
 }
---
+++ ConverterBinding/MainPageControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace ConverterBinding
+{
+    public partial class MainPageControl
+    {
+        public static readonly BindableProperty NullableBoolProperty = BindableProperty.Create(
+            nameof(NullableBool),
+            typeof(bool?),
+            typeof(MainPageControl));
+
+        public static readonly BindableProperty BoolProperty = BindableProperty.Create(
+            nameof(Bool),
+            typeof(bool),
+            typeof(MainPageControl));
+
+        public MainPageControl()
+        {
+            InitializeComponent();
+        }
+        
+        public bool? NullableBool
+        {
+            get => (bool?)GetValue(NullableBoolProperty);
+            set => SetValue(NullableBoolProperty, value);
+        }
+        
+        public bool Bool
+        {
+            get => (bool) GetValue(BoolProperty);
+            set => SetValue(BoolProperty, value);
+        }
+    }
+}
