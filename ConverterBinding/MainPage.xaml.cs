506437d0c15468f9331cf2398f9e0ca623051d94
Bryce Lynch <techbryce@hotmail.com>
2023-08-02 21:19:27 +1000

    ADD - Re-implemented AutomationProperties example.

--- ConverterBinding/MainPage.xaml.cs
+++ ConverterBinding/MainPage.xaml.cs
@@ -3,6 +3,8 @@
 public partial class MainPage
 {
     public string TextValue { get; set; }
+    public string AccessibilityName { get; set; } = string.Empty;
+    public bool IsInAccessibleTree { get; set; } = true;
     public MainPage()
     {
         InitializeComponent();
